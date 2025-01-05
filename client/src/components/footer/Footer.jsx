0a4d4a6ac19fd5d9e068a4a3a4f608932100cbd5
Rimrachai Marma <60957299+rimrachai-marma@users.noreply.github.com>
2025-01-05 13:44:11 +0600

    Update Footer.jsx

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -6,8 +6,7 @@ function Footer() {
     <footer id={styles.footer}>
       <Container>
         <p className={styles.footer}>
-          Copyright &copy; e-shop, {new Date().getFullYear()}. All rights
-          reserved.
+          &copy; e-shop, {new Date().getFullYear()}.. All rights reserved.
         </p>
       </Container>
     </footer>
